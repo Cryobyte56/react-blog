5d5bb46c36f942710d8eef46daeaad1816eeaa68
cryobyte56 <danielasanon54@gmail.com>
2025-03-28 09:28:34 +0800

    Added Tailwind and Shadcn Library

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0065a9",
+      },
+    },
+  },
+  plugins: [],
+};
+
